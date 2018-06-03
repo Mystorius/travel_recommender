49116b1842ec6339b4a8df9bf7650316db970a16
Mystorius <mystorius1808+github@gmail.com>
2018-06-03 23:26:47 +0200

    changed lsa model

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from calc_fit import calc_best_fit
 from calc_fit import query_db
 
+'''This section is just a user based input and my given outputs'''
 
 def Main():
     name = input("Please input country you like: ")
-    country_id = query_db("""Select id from countries where name like "{}" """.format(name))
+    country_id = query_db("""Select id from countries where name like '{}' """.format(name))
     type = input("Please input your favorite journey type. You get to choose from: culture, nightlife, activity: ")
     fit0, fit1, fit2 = calc_best_fit(country_id[0], type)
     print(fit0, fit1, fit2)
--- calc_fit.py
+++ calc_fit.py
@@ -9,6 +9,8 @@ db_path = r"sqlite.db"
 conn = sqlite3.connect(db_path)
 cursor = conn.cursor()
 
+
+## create table in db
 def create_table():
     try:
         conn = sqlite3.connect(db_path)
@@ -25,6 +27,7 @@ def create_table():
 #create_table()
 
 
+## insert matrix for activity types
 def insert_matrix(v1, v2, v3, v4, v5, v6, v7, v8, v9 ,v10, v11):
     try:
         conn = sqlite3.connect(db_path)
@@ -40,6 +43,7 @@ def insert_matrix(v1, v2, v3, v4, v5, v6, v7, v8, v9 ,v10, v11):
         conn.close()
 
 
+## creates matrix for activity types
 def create_matrix():
     matrix_old = {"culture":   [0.7, 0.5, 0.5, 0.5, 0.3, 1, 1, 0.3, 0.7, 0.3, 0.5],
               "nightlife": [0.2, 0.5, 0.5, 0.5, 0.5, 0.3, 0.3, 1, 1, 1, 0.7],
@@ -53,23 +57,7 @@ def create_matrix():
 #create_matrix()
 
 
-# def query_db(query):
-#     result = []
-#     try:
-#         conn = sqlite3.connect(db_path)
-#         cursor = conn.cursor()
-#         output = cursor.execute(query)
-#         conn.commit()
-#     except Error as e:
-#         print(e)
-#     finally:
-#         for i in output:
-#             for j in i:
-#                 result.append(j)
-#         conn.close()
-#         return result
-#
-
+## creates id for given type
 def get_type_id(type):
     if type == "culture":
         type_id = 1
@@ -82,7 +70,9 @@ def get_type_id(type):
     return type_id
 
 
-def calc_best_fit(id, type):
+## gets the best fit for input country out of db and returns top 3 matches
+
+def calc_best_fit_sim(id, type):
     country = query_db("""Select * from similarity where id = {}""".format(id))
     type_id = get_type_id(type)
     matrix = query_db("""Select * from matrix where id = {}""".format(type_id))
@@ -110,13 +100,50 @@ def calc_best_fit(id, type):
                                     shopping[1]*matrix[9]+
                                     nightlife[1]*matrix[10]+
                                     getting_around[1]*matrix[11])])
-    sum_list_sorted = sorted(sum_list, key=operator.itemgetter(1), reverse=True)
-    fit0 = query_db("""Select name from countries where id = {}""".format(sum_list_sorted[1][0]))
-    fit1 = query_db("""Select name from countries where id = {}""".format(sum_list_sorted[2][0]))
-    fit2 = query_db("""Select name from countries where id = {}""".format(sum_list_sorted[3][0]))
+    return sum_list
+
+def calc_best_fit_lsa(id, type):
+    country = query_db("""Select * from lsa_model where id = {}""".format(id))
+    type_id = get_type_id(type)
+    matrix = query_db("""Select * from matrix where id = {}""".format(type_id))
+    about = pickle.loads(country[2])
+    safety = pickle.loads(country[3])
+    terrorism = pickle.loads(country[4])
+    entry = pickle.loads(country[5])
+    health = pickle.loads(country[6])
+    history = pickle.loads(country[7])
+    culture = pickle.loads(country[8])
+    attractions = pickle.loads(country[9])
+    shopping = pickle.loads(country[10])
+    nightlife = pickle.loads(country[11])
+    getting_around = pickle.loads(country[12])
+    sum_list = []
+    for about, safety, terrorism, entry, health, history, culture, attractions, shopping, nightlife, getting_around in zip(about, safety, terrorism, entry, health, history, culture, attractions, shopping, nightlife, getting_around):
+        sum_list.append([about[0], (about[1]*matrix[1]+
+                                    safety[1]*matrix[2]+
+                                    terrorism[1]*matrix[3]+
+                                    entry[1]*matrix[4]+
+                                    health[1]*matrix[5]+
+                                    history[1]*matrix[6]+
+                                    culture[1]*matrix[7]+
+                                    attractions[1]*matrix[8]+
+                                    shopping[1]*matrix[9]+
+                                    nightlife[1]*matrix[10]+
+                                    getting_around[1]*matrix[11])])
+    return sum_list
+
+def calc_best_fit(id, type):
+    sum_list1 = calc_best_fit_sim(id, type)
+    sum_list2 = calc_best_fit_lsa(id, type)
+    sum_list3 = []
+    for i in sum_list2:
+        sum_list3.append([i[0],i[1]*-1])
+    sum_list = []
+    for x,y in zip(sum_list1, sum_list2):
+        sum_list.append([x[0], y[1]+y[1]])
+    sum_list_sorted = sorted(sum_list3, key=operator.itemgetter(1), reverse=True)
+
+    fit0 = query_db("""Select name from countries where id = {}""".format(sum_list_sorted[0][0]))
+    fit1 = query_db("""Select name from countries where id = {}""".format(sum_list_sorted[1][0]))
+    fit2 = query_db("""Select name from countries where id = {}""".format(sum_list_sorted[2][0]))
     return fit0, fit1, fit2
-
-#calc_best_fit(144, "culture")
-#calc_best_fit(144, "nightlife")
-#calc_best_fit(144, "activity")
-
