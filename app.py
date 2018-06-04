ba06854b2518d51e5699d7206fac49c72d63a3df
Benjamin Pohl <benjamin.pohl@de.ey.com>
2018-06-04 13:47:33 +0200

    changes made to docu

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def Main():
     country_id = query_db("""Select id from countries where name like '{}' """.format(name))
     type = input("Please input your favorite journey type. You get to choose from: culture, nightlife, activity: ")
     fit0, fit1, fit2 = calc_best_fit(country_id[0], type)
-    print(fit0, fit1, fit2)
+    print("Countries you might like to visit: {0}, {1} or {2}".format(fit0[0], fit1[0], fit2[0]))
 
 if __name__ == '__main__':
     while True:
